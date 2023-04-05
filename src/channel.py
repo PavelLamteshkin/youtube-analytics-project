cbc98ad3c7c524f6b280632e999da775271ced13
MummyHouse <lamteshkin@gmail.com>
2023-04-05 21:11:32 +0300

    Initial release

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,26 @@
-
+import os
+from googleapiclient.discovery import build
+import json
 
 class Channel:
+
     """Класс для ютуб-канала"""
 
+    api_key = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey='AIzaSyApgQ2Fl18ga8EA2Yegx5CAkWcJPhvsFAg')
+
+    # Функция build из googleapiclient.discovery позволяет создать ресурс для обращения к API,
+    # то есть это некая абстракция над REST API Drive, чтобы удобнее обращаться к методам API.
+
+
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+
+        self.channel_id = channel_id
+
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+
+        """Выводит в консоль информацию о канале в JSON."""
+
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))

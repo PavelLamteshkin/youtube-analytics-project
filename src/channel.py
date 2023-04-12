0b0de51480883c24e2d2df4e8ca985d0de70eb50
MummyHouse <lamteshkin@gmail.com>
2023-04-12 16:44:04 +0300

    К ДЗ2

--- src/channel.py
+++ src/channel.py
@@ -6,21 +6,41 @@ class Channel:
 
     """Класс для ютуб-канала"""
 
-    api_key = os.getenv('API_KEY')
-    youtube = build('youtube', 'v3', developerKey='AIzaSyApgQ2Fl18ga8EA2Yegx5CAkWcJPhvsFAg')
+    api_key = os.getenv('YOUTUBE_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     # Функция build из googleapiclient.discovery позволяет создать ресурс для обращения к API,
     # то есть это некая абстракция над REST API Drive, чтобы удобнее обращаться к методам API.
 
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id, title, description, url, subscriberCount, videoCount, viewCount) -> None:
 
         self.channel_id = channel_id
+        self.title = title
+        self.description = description
+        self.url = url
+        self.subscriberCount = subscriberCount
+        self.videoCount = videoCount
+        self.viewCount = viewCount
 
 
-    def print_info(self) -> None:
+    @classmethod
+    def get_service(cls, channel_id):
+        """
+        Метод возвращает объект для работы с YouTube API
+        """
+        # api_key = os.getenv('YOUTUBE_API_KEY')
+        # youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = cls.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
-        """Выводит в консоль информацию о канале в JSON."""
+        return channel
 
+
+    def to_json(self):
+        """
+        Метод сохраняет в файл значения атрибутов экземпляра
+        """
         channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        channel_json = json.dumps(channel, indent=2, ensure_ascii=False)
+
+        return channel_json

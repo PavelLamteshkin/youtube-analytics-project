f3f1a8e5b7184976fe9895d6473e93e513f50a13
MummyHouse <lamteshkin@gmail.com>
2023-04-16 16:21:55 +0300

    Внесение замечаний ко второй части ДЗ

--- src/channel.py
+++ src/channel.py
@@ -2,45 +2,32 @@ import os
 from googleapiclient.discovery import build
 import json
 
+
 class Channel:
 
     """Класс для ютуб-канала"""
 
-    api_key = os.getenv('YOUTUBE_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
-
-    # Функция build из googleapiclient.discovery позволяет создать ресурс для обращения к API,
-    # то есть это некая абстракция над REST API Drive, чтобы удобнее обращаться к методам API.
-
-
-    def __init__(self, channel_id, title, description, url, subscriberCount, videoCount, viewCount) -> None:
+    def __init__(self, channel_id) -> None:
 
         self.channel_id = channel_id
-        self.title = title
-        self.description = description
-        self.url = url
-        self.subscriberCount = subscriberCount
-        self.videoCount = videoCount
-        self.viewCount = viewCount
-
 
     @classmethod
-    def get_service(cls, channel_id):
+    def get_service(cls):
         """
         Метод возвращает объект для работы с YouTube API
         """
-        # api_key = os.getenv('YOUTUBE_API_KEY')
-        # youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = cls.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
-        return channel
+        api_key = os.getenv('YOUTUBE_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
 
+        return youtube
 
-    def to_json(self):
+    @classmethod
+    def to_json(cls, channel_id):
         """
         Метод сохраняет в файл значения атрибутов экземпляра
         """
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = cls.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
         channel_json = json.dumps(channel, indent=2, ensure_ascii=False)
 
         return channel_json

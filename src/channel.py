a82760ad9865296d8ecd7733f34ce75c2e33092f
MummyHouse <lamteshkin@gmail.com>
2023-04-17 13:11:37 +0300

    Третье внесение замечаний ко второй части ДЗ

--- src/channel.py
+++ src/channel.py
@@ -7,9 +7,13 @@ class Channel:
 
     """Класс для ютуб-канала"""
 
+    api_key = os.getenv('YOUTUBE_API_KEY')
+
+
     def __init__(self, channel_id) -> None:
 
         self.channel_id = channel_id
+
 
     @classmethod
     def get_service(cls):
@@ -17,10 +21,10 @@ class Channel:
         Метод возвращает объект для работы с YouTube API
         """
 
-        api_key = os.getenv('YOUTUBE_API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
+        youtube = build('youtube', 'v3', developerKey=cls.api_key)
 
         return youtube
+
 
     @classmethod
     def to_json(cls, channel_id):
@@ -28,6 +32,17 @@ class Channel:
         Метод сохраняет в файл значения атрибутов экземпляра
         """
         channel = cls.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
-        channel_json = json.dumps(channel, indent=2, ensure_ascii=False)
 
-        return channel_json
+        cls.title = channel['items'][0]['snippet']['title']
+        cls.description = channel['items'][0]['snippet']['description']
+        cls.url = channel['items'][0]['snippet']['thumbnails']['default']['url']
+        cls.subscriberCount = channel['items'][0]['statistics']['subscriberCount']
+        cls.videoCount = channel['items'][0]['statistics']['videoCount']
+        cls.viewCount = channel['items'][0]['statistics']['viewCount']
+
+        return channel
+
+
+    def print_info(self, channel):
+
+        return json.dumps(channel, indent=2, ensure_ascii=False)

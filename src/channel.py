daf00a69caa5477af3104d8f53a820307c82909e
MummyHouse <lamteshkin@gmail.com>
2023-04-23 13:04:35 +0300

    Initial. Вторая часть ДЗ_4

--- src/channel.py
+++ src/channel.py
@@ -9,7 +9,6 @@ class Channel:
 
     api_key = os.getenv('YOUTUBE_API_KEY')
 
-
     def __init__(self, channel_id) -> None:
 
         self.channel_id = channel_id
@@ -17,16 +16,13 @@ class Channel:
         channel = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
         self.title = channel['items'][0]['snippet']['title']
         self.description = channel['items'][0]['snippet']['description']
-        # self.url = channel['items'][0]['snippet']['thumbnails']['default']['url']
         self.url = 'https://www.youtube.com/channel/' + self.channel_id
         self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
         self.video_count = channel['items'][0]['statistics']['videoCount']
         self.view_count = channel['items'][0]['statistics']['viewCount']
 
-
     def __str__(self):
         return f'{self.title} ({self.url})'
-
 
     @classmethod
     def get_service(cls):
@@ -37,7 +33,6 @@ class Channel:
         youtube = build('youtube', 'v3', developerKey=cls.api_key)
 
         return youtube
-
 
     def to_json(self, jason_file):
         """
@@ -57,35 +52,27 @@ class Channel:
 
         return data
 
-
     def print_info(self, data):
 
         return json.dumps(data, indent=2, ensure_ascii=False)
 
-
     def __add__(self, other):
         return int(self.subscriber_count) + int(other.subscriber_count)
-
 
     def __sub__(self, other):
         return int(self.subscriber_count) - int(other.subscriber_count)
 
-
     def __gt__(self, other):
         return int(self.subscriber_count) > int(other.subscriber_count)
-
 
     def __ge__(self, other):
         return int(self.subscriber_count) >= int(other.subscriber_count)
 
-
     def __lt__(self, other):
         return int(self.subscriber_count) < int(other.subscriber_count)
 
-
     def __le__(self, other):
         return int(self.subscriber_count) <= int(other.subscriber_count)
-
 
     def __eq__(self, other):
         return int(self.subscriber_count) == int(other.subscriber_count)
---
+++ src/video.py
@@ -0,0 +1,53 @@
+from src.channel import Channel
+import os
+from googleapiclient.discovery import build
+
+
+class Video:
+
+    api_key = os.getenv('YOUTUBE_API_KEY')
+
+    def __init__(self, video_id):
+        self.video_id = video_id
+        video = self.get_service().videos().list(id=video_id, part='snippet,statistics').execute()
+        self.title = video['items'][0]['snippet']['title']
+        self.url = 'https://www.youtube.com/watch?v=' + self.video_id
+        self.view_count = video['items'][0]['statistics']['viewCount']
+        self.like_count = video['items'][0]['statistics']['likeCount']
+
+
+    @classmethod
+    def get_service(cls):
+        """
+        Метод возвращает объект для работы с YouTube API
+        """
+
+        youtube = build('youtube', 'v3', developerKey=cls.api_key)
+
+        return youtube
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class PLVideo(Video):
+
+    def __init__(self, video_id, list_id):
+        super().__init__(video_id)
+        self.list_id = list_id
+
+
+# api_key = os.getenv('YOUTUBE_API_KEY')
+# channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'
+# youtube = build('youtube', 'v3', developerKey=api_key)
+# channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+# print(channel)
+
+# video1 = Video('9lO06Zxhu88')
+# api_key = os.getenv('YOUTUBE_API_KEY')
+# youtube = build('youtube', 'v3', developerKey=api_key)
+# channel = youtube.videos().list(id='9lO06Zxhu88', part='snippet,statistics').execute()
+# print(channel)
+
+# video1 = Video('BBotskuyw_M')
+# print(video1.url)

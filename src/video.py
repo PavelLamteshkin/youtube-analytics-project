bae3ecbc30b5f232863599e4e3969467f2c73bf0
MummyHouse <lamteshkin@gmail.com>
2023-05-12 17:07:29 +0300

    Initial. Вторая часть ДЗ_6

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 import os
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class Video:
@@ -8,12 +9,20 @@ class Video:
 
     def __init__(self, video_id):
         self.video_id = video_id
-        video = self.get_service().videos().list(id=video_id, part='snippet,statistics').execute()
-        self.title = video['items'][0]['snippet']['title']
-        self.url = 'https://www.youtube.com/watch?v=' + self.video_id
-        self.view_count = video['items'][0]['statistics']['viewCount']
-        self.like_count = video['items'][0]['statistics']['likeCount']
-
+        try:
+            video = self.get_service().captions().list(videoId=video_id, part='snippet').execute()
+        except HttpError as e:
+            print(f'wrong video_ID, error {e.status_code}')
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+        else:
+            video = self.get_service().videos().list(id=video_id, part='snippet,statistics').execute()
+            self.title = video['items'][0]['snippet']['title']
+            self.url = 'https://www.youtube.com/watch?v=' + self.video_id
+            self.view_count = video['items'][0]['statistics']['viewCount']
+            self.like_count = video['items'][0]['statistics']['likeCount']
 
     @classmethod
     def get_service(cls):
@@ -34,19 +43,3 @@ class PLVideo(Video):
     def __init__(self, video_id, list_id):
         super().__init__(video_id)
         self.list_id = list_id
-
-
-# api_key = os.getenv('YOUTUBE_API_KEY')
-# channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'
-# youtube = build('youtube', 'v3', developerKey=api_key)
-# channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-# print(channel)
-
-# video1 = Video('9lO06Zxhu88')
-# api_key = os.getenv('YOUTUBE_API_KEY')
-# youtube = build('youtube', 'v3', developerKey=api_key)
-# channel = youtube.videos().list(id='9lO06Zxhu88', part='snippet,statistics').execute()
-# print(channel)
-
-# video1 = Video('BBotskuyw_M')
-# print(video1.url)
